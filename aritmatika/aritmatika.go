acde5cef336aae7359da8f20badc2197bbde2674
Iskandar <muhamad.siskandar.zulkarnain@gmail.com>
2024-10-21 11:29:41 +0700

    aritmatika

---
+++ aritmatika/aritmatika.go
@@ -0,0 +1,26 @@
+package aritmatika
+
+// penjumlahan
+func AritmatikaPenjumlahan(a int, b int) int {
+	return a + b
+}
+
+// perngurangan
+func AritmatikaPengurangan(a int, b int) int {
+	return a - b
+}
+
+// perkalian
+func AritmatikaPerkalian(a int, b int) int {
+	return a * b
+}
+
+// perngurangan
+func AritmatikaPembagian(a int, b int) int {
+	return a / b
+}
+
+// sisa bagi
+func AritmatikaSisaBagi(a int, b int) int {
+	return a % b
+}
